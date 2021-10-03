f51cc2369f5436598bf9987c8c40866a7d15e3ef
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-03 19:55:21 +0200

    feat: Create Routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import LastArticles from './components/LastArticles.vue'
+import MyComponent from './components/MyComponent.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/home', component: LastArticles},
+  {path: '/last-articles', component: LastArticles},
+  {path: '/my-components', component: MyComponent},
+  {path: '/', component: LastArticles}
+];
